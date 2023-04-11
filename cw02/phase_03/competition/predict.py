3a048a91ed5df9f101882bcc65d69593e8e5dc92
Johnny Foulds <hfoulds@gmail.com>
2023-04-11 19:53:34 +0200

    Predicting question 11

--- cw02/phase_03/competition/predict.py
+++ cw02/phase_03/competition/predict.py
@@ -4,10 +4,13 @@ This module implements a base class to use for prediction.
 
 from abc import ABC, abstractmethod
 import logging
-from typing import List
+from typing import List, Dict, Any
 import pandas as pd
 import numpy as np
 from tqdm.auto import tqdm
+
+from competition import feature_engineering as fe
+from competition import model_data as md
 
 # create an abstract base class
 class PredictionBase(ABC):
@@ -103,3 +106,117 @@ class Baseline(PredictionBase):
         """
         return self.answer_key[question_num - 1]
 
+
+class HeatmapPredictor(Baseline):
+    """
+    Use the best model for question 5 to predict the correct labels.
+    """
+
+    def __init__(self, models:Dict[int, Dict[Any, float]]):
+        # call the base class constructor
+        super().__init__()
+
+        # initialize the models collection
+        self.models = {}
+        self.thresholds = {}
+
+        # assign the models
+        self.models = models
+
+    def feature_engineering(self, data:pd.DataFrame, labels:pd.DataFrame) -> pd.DataFrame:
+        """
+        This method is used to perform feature engineering on the data.
+        """
+        logging.info('Performing feature engineering:')
+        # create the initial features
+        df_features = fe.create_initial_features(data, labels)
+
+        # add the elapsed time feature to the features dataset
+        logging.info('\tAdding elapsed time features...')
+        df_features = fe.add_elapsed_time_features(
+            features=df_features,
+            X=data)
+
+        # add the total count features to the features dataset
+        logging.info('\tAdding total count features...')
+        df_features = fe.add_count_total_features(
+            features=df_features,
+            X=data)
+
+        # add the unique count features to the features dataset
+        logging.info('\tAdding unique count features...')
+        df_features = fe.add_count_unique_features(
+            features=df_features,
+            X=data)
+
+        # add the heatmap features to the features dataset
+        logging.info('\tAdding heatmap features...')
+        df_features = fe.add_screen_heatmap_feature(
+            features=df_features,
+            X=data,
+            verbose=True)
+        
+        return df_features
+
+    def create_feature_dataset(self,
+                               features:pd.DataFrame,
+                               labels:pd.DataFrame) -> List[np.ndarray]:
+        """
+        This method is used to create a feature dataset.
+        """   
+        # create the flat features dataset
+        features_dataset = md.create_feature_dataset(
+            df_features=features,
+            df_source_labels=labels,
+            session_list=labels.session_id.unique(),
+            feature_list=['elapsed_time_sum', 'elapsed_time_max', 
+                          'elapsed_time_min', 'elapsed_time_mean', 
+                          'elapsed_time_mode'],
+            include_question=True,
+            expand_question=False,
+            verbose=False)
+
+        # create the heatmap features dataset
+        #logging.info('Creating the heatmap features dataset...')
+        heatmap_dataset = md.create_feature_dataset(
+            df_features=features,
+            df_source_labels=labels,
+            session_list=labels.session_id.unique(),
+            feature_list=['screen_heatmap_feature'],
+            include_question=False,
+            expand_question=False,
+            verbose=False)
+
+        return [heatmap_dataset, features_dataset]
+
+
+    def predict_question(self, feature_set:List[np.ndarray], question_num:int) -> int:
+        """
+        Predict the correct answer for the given question.
+
+        Parameters
+        ----------
+        feature_set : List[pd.DataFrame]
+            The list of feature sets for the questions.
+        question_num : int
+            The question number to predict.
+
+        Returns
+        -------
+        int
+            The predicted answer for the question.
+        """
+        # if no model is defined for the question, use the base class
+        model_data = self.models.get(question_num, None)
+        if model_data is None:
+            return super().predict_question(feature_set, question_num)
+
+        # get the model and threshold
+        model = model_data['model']
+        threshold = model_data['threshold']
+
+        # use the model for prediction
+        y_pred_model = model.predict(feature_set, verbose=0)
+        y_pred_model = (y_pred_model[:, 1] > threshold).astype(int)
+
+        return y_pred_model[0]
